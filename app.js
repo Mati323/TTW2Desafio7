b84f7f1074700fcf5556862e625c2b13734d47cf
mati323 <matiassoler07@gmail.com>
2025-05-21 12:04:28 -0300

    -

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const precioProducto = document.getElementById("precioProducto")
 
 precioProducto.innerText = "$450"
 
-const nombreProducto = document.querySelector("#nombreProducto")
+ const nombreProducto = document.querySelector("#nombreProducto")
 
-nombreProducto.style.color = "bisque"
+nombreProducto.style.color = "green"
